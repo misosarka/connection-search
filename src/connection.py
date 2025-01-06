72f0d980c0aad7fb47a3ef8d363157639cec6650
misosarka <michal.bernat.2005@gmail.com>
2025-01-06 23:21:51 +0100

    Add methods to Connections and describe ConnectionQuality

--- src/connection.py
+++ src/connection.py
@@ -1,13 +1,25 @@
 from dataclasses import dataclass
+from datetime import date, datetime, time
 from .structures import Route, Stop, StopTime, Trip
+
+
+MIDNIGHT = time(0, 0)
 
 
 @dataclass
 class TripConnectionSegment:
-    """A single segment of a connection representing a single trip."""
+    """
+    A single segment of a connection representing a single trip.
+    
+    The start_stoptime and end_stoptime represent where and when the user should get on and get off.
+    The service_day represents a date for which the trip runs, as in trips.txt.
+    For example, if the trip was from 0:30 to 1:00 on Tuesday, but the dataset described it as a trip
+    on Monday from 24:30 to 25:00, the service_day would be the date for Monday.
+    """
 
     start_stoptime: StopTime
     end_stoptime: StopTime
+    service_day: date
 
     @property
     def trip(self) -> Trip:
@@ -26,10 +38,16 @@ class TripConnectionSegment:
         return self.trip.get_route()
 
 
+@dataclass
 class OpenTripConnectionSegment:
-    """An open segment of a connection representing a trip with no defined end stop."""
+    """
+    An open segment of a connection representing a trip with no defined end stop.
+    
+    For field explanations, see TripConnectionSegment.
+    """
     
     start_stoptime: StopTime
+    service_day: date
 
     @property
     def trip(self) -> Trip:
@@ -50,6 +68,20 @@ class Connection:
 
     segments: list[TripConnectionSegment]
 
+    @classmethod
+    def empty(cls) -> "Connection":
+        return cls([])
+    
+    def to_open_connection(self, last_departure: StopTime, last_departure_service_day: date) -> "OpenConnection":
+        return OpenConnection(self.segments, OpenTripConnectionSegment(last_departure, last_departure_service_day))
+    
+    @property
+    def first_departure(self) -> datetime | None:
+        if not self.segments:
+            return None
+        first_stoptime_departure = self.segments[0].start_stoptime.departure_time
+        return datetime.combine(self.segments[0].service_day, MIDNIGHT) + first_stoptime_departure
+
 
 @dataclass
 class OpenConnection:
@@ -57,3 +89,31 @@ class OpenConnection:
 
     segments: list[TripConnectionSegment]
     final_segment: OpenTripConnectionSegment
+
+    def to_connection(self, last_arrival: StopTime) -> Connection:
+        last_segment = TripConnectionSegment(self.final_segment.start_stoptime, last_arrival, self.final_segment.service_day)
+        return Connection(self.segments + [last_segment])
+
+    @property
+    def first_departure(self) -> datetime:
+        if not self.segments:
+            first_stoptime_departure = self.final_segment.start_stoptime.departure_time
+            first_segment_service_day = self.final_segment.service_day
+        else:
+            first_stoptime_departure = self.segments[0].start_stoptime.departure_time
+            first_segment_service_day = self.segments[0].service_day
+        return datetime.combine(first_segment_service_day, MIDNIGHT) + first_stoptime_departure
+
+
+@dataclass
+class ConnectionQuality:
+    """
+    A measure of quality of Connections and OpenConnections.
+
+    Only instances with the same endpoint (stop for Connections and trip for OpenConnections) should be compared.
+    The comparison is defined as follows:
+    1. The instance with the later first departure time is better (meaning its ConnectionQuality is higher).
+    2. If both instances have the same first departure time, the instance with fewer transfers is better.
+    3. If the first departure times and transfer counts are both the same, the qualities are equal.
+    """
+    # TODO
